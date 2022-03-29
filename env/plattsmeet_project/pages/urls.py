69b3553d482ae20d3ce78cc8ca227abfa74697c8
Xliu008 <57504740+Xliu008@users.noreply.github.com>
2022-03-29 16:18:52 -0400

    Update urls.py

--- env/plattsmeet_project/pages/urls.py
+++ env/plattsmeet_project/pages/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
    path('help/', HelpPageView.as_view(),name = 'help'),
    path('portalpage/', PortalPageView.as_view(),name = 'portalpage'),
    path('registeration/', RegistrationPageView.as_view(),name = 'registeration'),
+   path('password_reset/', RegistrationPageView.as_view(),name = 'password_reset_form'),
 ]
