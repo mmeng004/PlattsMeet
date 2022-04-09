618eaa2e118dd7b0268607a3792e35a00831c06e
mmeng004 <73732407+mmeng004@users.noreply.github.com>
2022-04-09 12:47:34 -0400

    Delete env directory

--- env/plattsmeet_project/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-from . import views
-urlpatterns = [
-   path("register/", views.register_request, name="register"),
-   #path('registeration/', RegistrationPageView.as_view(),name = 'registeration'),
-]
