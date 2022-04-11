71d41eedf4fbccf39120946e67d4fc28ecca4c79
Meron Mengistu <mmeng004@plattsburgh.edu>
2022-04-11 16:01:11 -0400

    Update Settings

--- dock/plattsmeet/settings.py
+++ dock/plattsmeet/settings.py
@@ -74,18 +74,16 @@ WSGI_APPLICATION = 'plattsmeet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
-        'PORT': 5432,
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
